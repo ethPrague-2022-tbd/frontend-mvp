6cc7f4d118d1dd6a90ade8b7cf2a9629d833ec81
David Recheni <david@lhotse.de>
2022-06-10 18:59:21 +0200

    Router

--- myspace-3/src/App.tsx
+++ myspace-3/src/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import Topbar from './components/Topbar';
-import UserProfile from './components/UserProfile';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Topbar from "./components/Topbar";
+import UserProfile from "./components/UserProfile";
 
 const App: React.FC = () => (
   <div className="bg-red-300 h-screen">
     <Topbar />
-    <UserProfile />
+    <Outlet />
   </div>
 );
 
--- myspace-3/src/main.tsx
+++ myspace-3/src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import React from "react";
+import "./index.css";
+import App from "./App";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import UserProfile from "./components/UserProfile";
+import Home from "./components/Home";
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="/" element={<Home />} />
+          <Route path="user" element={<UserProfile />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 );
