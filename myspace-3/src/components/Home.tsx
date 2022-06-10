a685c6fd6e2cab952f70e0cfb0f44058bcdd71ed
David Recheni <david@lhotse.de>
2022-06-10 19:14:26 +0200

    Updated profile view

--- myspace-3/src/components/Home.tsx
+++ myspace-3/src/components/Home.tsx
@@ -1,7 +1,7 @@
-import React from "react";
-
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="bg-slate-300 h-full p-4 flex w-screen gap-4">Home</div>
+  );
 };
 
 export default Home;
--- myspace-3/src/main.tsx
+++ myspace-3/src/main.tsx
@@ -5,6 +5,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserProfile from "./components/UserProfile";
 import Home from "./components/Home";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
