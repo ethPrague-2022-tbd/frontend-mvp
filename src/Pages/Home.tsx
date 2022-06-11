4e9c2abb08181b6b6dc6f6114c85a2cf6a520a77
David Recheni <david@lhotse.de>
2022-06-11 17:02:17 +0200

    Popular users

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,9 +1,11 @@
 import PopularGroups from "../components/PopularGroups";
+import PopularUsers from "../components/PopularUsers";
 
 const Home = () => {
   return (
     <div className="bg-black min-h-screen h-fit flex w-screen gap-4 p-4 text-white">
       <PopularGroups />
+      <PopularUsers />
     </div>
   );
 };
--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -5,7 +5,7 @@ const Topbar = () => {
   return (
     <div className="flex bg-black text-white py-2 border-b border-slate-300 p-4 justify-between">
       <Link to="/">
-        <p className="text-3xl">tbd</p>
+        <p className="text-3xl">tbd.</p>
       </Link>
       <input type="text" placeholder="search" className="p-2 w-64 text-black" />
       <div className="flex">
