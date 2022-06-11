f1f631103c9e8f5bf1b4952c7183e24780645588
David Recheni <david@lhotse.de>
2022-06-11 15:33:11 +0200

    WIP

--- src/components/Repositories.tsx
+++ src/components/Repositories.tsx
@@ -2,7 +2,7 @@ const Repositories = () => {
   return (
     <div className="flex flex-col gap-2 h-full text-white">
       <p>Repositories</p>
-      <div className="h-full w-full flex flex-col gap-2 items-center justify-center  text-white">
+      <div className="h-full w-full flex flex-col gap-2  text-white">
         <div className="p-4 border rounded-lg cursor-pointer">
           <p className="font-medium">vbuterin/ethprague-mvp</p>
           <p className="text-slate-300 text-sm">
