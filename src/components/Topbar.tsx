2c4e5d9c8c241b3db7043a01aba259d94788b29c
David Recheni <david@lhotse.de>
2022-06-11 14:25:21 +0200

    Added connect button

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,12 +1,20 @@
 import { FaCog } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Topbar = () => {
   return (
     <div className="flex bg-black text-white py-2 border-b border-slate-300 p-4 justify-between">
-      <p className="text-3xl">tbd</p>
-      <input type="text" placeholder="search" className="p-2 w-64" />
-      <div className="px-3 py-2 flex items-center">
-        <FaCog size={22} />
+      <Link to="/">
+        <p className="text-3xl">tbd</p>
+      </Link>
+      <input type="text" placeholder="search" className="p-2 w-64 text-black" />
+      <div className="flex">
+        <button className="px-3 py-2 flex items-center text-white border border-white">
+          Connect wallet
+        </button>
+        <div className="px-3 py-2 flex items-center">
+          <FaCog size={22} />
+        </div>
       </div>
     </div>
   );
