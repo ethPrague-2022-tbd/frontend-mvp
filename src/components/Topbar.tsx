61a3c1760cd182e16218eca02851d95eea4c9d08
David Recheni <david@lhotse.de>
2022-06-11 12:55:36 +0200

    Profile update

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,7 +1,13 @@
+import { FaCog } from "react-icons/fa";
+
 const Topbar = () => {
   return (
-    <div className="bg-slate-800 text-white py-2 border-b border-slate-300">
-      Topbar
+    <div className="flex bg-black text-white py-2 border-b border-slate-300 p-4 justify-between">
+      <p className="text-3xl">tbd</p>
+      <input type="text" placeholder="search" className="p-2 w-64" />
+      <div className="px-3 py-2 flex items-center">
+        <FaCog size={22} />
+      </div>
     </div>
   );
 };
--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -1,41 +1,104 @@
 import profilePic from "../assets/profile-pic.jpeg";
-import { FaTelegramPlane, FaDiscord, FaTwitter } from "react-icons/fa";
+import {
+  FaTelegramPlane,
+  FaDiscord,
+  FaTwitter,
+  FaGithub,
+} from "react-icons/fa";
+
+import twit from "../assets/twit.png";
 const UserProfile = () => {
   return (
-    <div className="bg-slate-800 h-[95vh] flex w-screen gap-4">
-      <div className="flex flex-col gap-y-4 w-2/6 items-center border-r border-slate-300">
-        <div className="flex flex-col gap-y-4 w-full items-center p-2">
+    <div className="bg-black h-fit flex w-screen gap-4">
+      <div className="flex flex-col gap-y-4 w-2/6 items-center">
+        <div className="flex flex-col gap-y-4 w-full p-4 text-white">
           <img
             src={profilePic}
             alt="Profile pic"
-            className="rounded-full w-56"
+            className="rounded-full w-80"
           />
+          <div className="flex flex-col">
+            <p className="text-2xl font-semibold">Anonymous</p>
+            <p className="text-slate-400">@vbuterin</p>
+          </div>
+          <p>Developer / Bitcoin enthusiast</p>
           <div className="flex gap-4">
-            <div className="px-3 py-2 bg-white rounded-lg">
+            <div className="px-3 py-2 border border-white rounded-lg">
+              <FaGithub />
+            </div>
+            <div className="px-3 py-2 border border-white rounded-lg">
               <FaTelegramPlane />
             </div>
-            <div className="px-3 py-2 bg-white rounded-lg">
+            <div className="px-3 py-2 border border-white rounded-lg">
               <FaDiscord />
             </div>
-            <div className="px-3 py-2 bg-white rounded-lg">
+            <div className="px-3 py-2 border border-white rounded-lg">
               <FaTwitter />
             </div>
           </div>
-          <div className="bg-white h-96 w-full flex items-center justify-center">
-            Connect Github +
+          <div className="flex flex-col gap-2">
+            <p>Repositories</p>
+            <div className="h-full w-full flex flex-col gap-2 items-center justify-center  text-white">
+              <div className="p-4 border rounded-lg">
+                <p className="font-medium">vbuterin/ethprague-mvp</p>
+                <p className="text-slate-300 text-sm">
+                  Frontend code for EthPrague 2022 hackathon
+                </p>
+              </div>
+
+              <div className="p-4 border rounded-lg">
+                <p className="font-medium">vbuterin/ethprague-mvp</p>
+                <p className="text-slate-300 text-sm">
+                  Frontend code for EthPrague 2022 hackathon
+                </p>
+              </div>
+
+              <div className="p-4 border rounded-lg">
+                <p className="font-medium">vbuterin/ethprague-mvp</p>
+                <p className="text-slate-300 text-sm">
+                  Frontend code for EthPrague 2022 hackathon
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
       <div className="w-full pt-4 pr-4">
-        <div className="bg-white w-full h-36 flex items-center justify-center">
-          Connect to opensea+
-        </div>
         <div className="flex gap-4 pt-4">
-          <div className="w-4/6 bg-white h-[487px] flex items-center justify-center">
-            Twitter?
+          <div className="w-4/6 text-white flex flex-col ">
+            <p>Twitter</p>
+            <img src={twit} alt="twit" className="w-full h-auto" />
+            <img src={twit} alt="twit" className="w-full h-auto" />
+            <img src={twit} alt="twit" className="w-full h-auto" />
+            <img src={twit} alt="twit" className="w-full h-auto" />
           </div>
-          <div className="w-2/6 bg-white h-[487px] flex items-center justify-center">
-            Who saw my profile?
+          <div className="w-2/6 text-white flex flex-col gap-2 ">
+            <p>Groups</p>
+            <div className="p-4 border gap-2 rounded-lg flex w-full">
+              <img
+                src="https://miro.medium.com/max/3150/2*X_8LYIcePYE1DMhi8vcHFw.png"
+                alt="the lao"
+                className="w-16"
+              />
+              <div className="flex flex-col">
+                <p className="font-medium">The Lao</p>
+                <p className="text-slate-300 text-sm">
+                  A DAO SUPPORTING THE BLOCKCHAIN ECOSYSTEM
+                </p>
+              </div>
+            </div>
+
+            <div className="p-4 border gap-2 rounded-lg flex w-full">
+              <img
+                src="https://images.squarespace-cdn.com/content/v1/5c598e4ebfba3e5984563a0e/1565305053540-E97I3TTHCYHFU0DMW342/meta_chill+copy.png"
+                alt="Metacartel"
+                className="w-16"
+              />
+              <div className="flex flex-col">
+                <p className="font-medium">Metacartel</p>
+                <p className="text-slate-300 text-sm">el cartel de la meta</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
